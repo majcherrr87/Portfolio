e0f9af1ef03556d010865ae2d588f9b8bdd60c6c
Adrian Majcher <majcherrr87@gmail.com>
2022-10-24 22:07:58 +0200

    add timeline to Cv component

--- src/components/cv/Cv.jsx
+++ src/components/cv/Cv.jsx
@@ -36,16 +36,180 @@ export const Cv = () => {
                                 <h3 className="main-title">Skills</h3>
                                 <ul>
                                     <li>
-                                        <h5 className="skill-title">JavaScript</h5>
+                                        <p className="skill-title">JavaScript</p>
+                                        <div className="progress-bar">
+                                            <div className="progress js-progress"></div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <p className="skill-title">JavaScript</p>
+                                        <div className="progress-bar">
+                                            <div className="progress js-progress"></div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <p className="skill-title">Javaipt</p>
+                                        <div className="progress-bar">
+                                            <div className="progress js-progress"></div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <p className="skill-title">JavaScripvvcxvt</p>
                                         <div className="progress-bar">
                                             <div className="progress js-progress"></div>
                                         </div>
                                     </li>
                                 </ul>
                             </div>
+                            <div className="references-section">
+                                <h3 className="main-title">References</h3>
+                                <div className="referee">
+                                    <h6 className="sub-title">Michael Bowen</h6>
+                                    <p className='sub-para'>Product Manager</p>
+                                    <ul>
+                                        <li>
+                                            <FaGithub className='cv-icon'/>
+                                            513 354 369
+                                        </li>
+                                        <li>
+                                            <FaGithub className='cv-icon'/>
+                                            majcherrr87@gmail.com
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="referee">
+                                    <h6 className="sub-title">Michael Bowen</h6>
+                                    <p className='sub-para'>Product Manager</p>
+                                    <ul>
+                                        <li>
+                                            <FaGithub className='cv-icon'/>
+                                            513 354 369
+                                        </li>
+                                        <li>
+                                            <FaGithub className='cv-icon'/>
+                                            majcherrr87@gmail.com
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="referee">
+                                    <h6 className="sub-title">Michael Bowen</h6>
+                                    <p className='sub-para'>Product Manager</p>
+                                    <ul>
+                                        <li>
+                                            <FaGithub className='cv-icon'/>
+                                            513 354 369
+                                        </li>
+                                        <li>
+                                            <FaGithub className='cv-icon'/>
+                                            majcherrr87@gmail.com
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </section>
-                    <section className="right-section"></section>
+                    <section className="right-section">
+                        <div className="right-main-content">
+                            <section className="about sect">
+                                <h2 className="right-title">About Me</h2>
+                                <p className="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
+                                    delectus dignissimos dolorem quas. Assumenda expedita molestias possimus quasi
+                                    veniam. Aperiam laborum perspiciatis porro provident veniam voluptatum. Dolores
+                                    iusto quidem velit. <br/> <br/>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam commodi dolore
+                                    dolorum impedit molestias natus, neque obcaecati odit, qui quidem quis quo tenetur
+                                    vel
+                                </p>
+                            </section>
+                            <section className="experience sect">
+                                <h2 className="right-title">Experience</h2>
+                                <div className="timeline">
+                                    <div className="left-tl-content">
+                                        <h5 className="tl-title">Microsoft</h5>
+                                        <p className="para">2001 - 2003</p>
+                                    </div>
+                                    <div className="right-tl-content">
+                                        <div className="tl-content">
+                                            <h5 className="tl-title-2">Junior Dev</h5>
+                                            <p className="para">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                                elit. Consequuntur deserunt earum et voluptates? Accusamus aliquid autem
+                                                cupiditate deleniti dolores eius est ex, inventore reprehenderit sint
+                                                ullam ut veniam veritatis voluptates.</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div className="timeline">
+                                    <div className="left-tl-content">
+                                        <h5 className="tl-title">Microsoft</h5>
+                                        <p className="para">2001 - 2003</p>
+                                    </div>
+                                    <div className="right-tl-content">
+                                        <div className="tl-content">
+                                            <h5 className="tl-title-2">Junior Dev</h5>
+                                            <p className="para">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                                elit. Consequuntur deserunt earum et voluptates? Accusamus aliquid autem
+                                                cupiditate deleniti dolores eius est ex, inventore reprehenderit sint
+                                                ullam ut veniam veritatis voluptates.</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div className="timeline">
+                                    <div className="left-tl-content">
+                                        <h5 className="tl-title">Microsoft</h5>
+                                        <p className="para">2001 - 2003</p>
+                                    </div>
+                                    <div className="right-tl-content">
+                                        <div className="tl-content">
+                                            <h5 className="tl-title-2">Junior Dev</h5>
+                                            <p className="para">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                                elit. Consequuntur deserunt earum et voluptates? Accusamus aliquid autem
+                                                cupiditate deleniti dolores eius est ex, inventore reprehenderit sint
+                                                ullam ut veniam veritatis voluptates.</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </section>
+                            <section className="education sect">
+                                <h2 className="right-title">Education</h2>
+                                <div className="timeline">
+                                    <div className="left-tl-content">
+                                        <h5 className="tl-title">Microsoft</h5>
+                                        <p className="para">2001 - 2003</p>
+                                    </div>
+                                    <div className="right-tl-content">
+                                        <div className="tl-content">
+                                            <h5 className="tl-title-2">Junior Dev</h5>
+                                            <p className="para">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                                elit. Consequuntur deserunt earum et voluptates? Accusamus aliquid autem
+                                                cupiditate deleniti dolores eius est ex, inventore reprehenderit sint
+                                                ullam ut veniam veritatis voluptates.</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div className="timeline">
+                                    <div className="left-tl-content">
+                                        <h5 className="tl-title">Microsoft</h5>
+                                        <p className="para">2001 - 2003</p>
+                                    </div>
+                                    <div className="right-tl-content">
+                                        <div className="tl-content">
+                                            <h5 className="tl-title-2">Junior Dev</h5>
+                                            <p className="para">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                                elit. Consequuntur deserunt earum et voluptates? Accusamus aliquid autem
+                                                cupiditate deleniti dolores eius est ex, inventore reprehenderit sint
+                                                ullam ut veniam veritatis voluptates.</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </section>
+
+                        </div>
+                    </section>
                 </main>
             </div>
         </>
